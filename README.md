ca5fada27dc9a8bc583b315dd5aa6eab7e5dd539
Hadi T <hadi@syniol.com>
2023-11-04 21:51:42 +0000

    Updated docs and comments for publication

--- README.md
+++ README.md
@@ -30,19 +30,19 @@ you could see JSON output below:
 ### Log levels
 Log levels for software applications have a rich history dating back to the 1980s. One of the earliest and most 
 influential logging solutions for Unix systems, [Syslog](https://en.wikipedia.org/wiki/Syslog) , introduced a range 
-of severity levels, which provided the first standardized framework for categorizing log entries based on their 
+of severity levels, which provided the first standardised framework for categorising log entries based on their 
 impact or urgency.
 
 The following are the levels defined by Syslog in descending order of severity:
 
- * Emergency `(emerg/fatal)`: _indicates that the system is unusable and requires immediate attention._
- * Alert `(alert)`: _indicates that immediate action is necessary to resolve a critical issue._
- * Critical `(crit)`: _signifies critical conditions in the program that demand intervention to prevent system failure._
- * Error `(error)`: _indicates error conditions that impair some operation but are less severe than critical situations._
- * Warning `(warn)`: _signifies potential issues that may lead to errors or unexpected behavior in the future if not addressed._
- * Notice `(notice)`: _applies to normal but significant conditions that may require monitoring._
- * Informational `(info)`: _includes messages that provide a record of the normal operation of the system._
- * Debug `(debug)`: _intended for logging detailed information about the system for debugging purposes._
+ * __Emergency__ `(emerg/fatal)`: _System is unusable and requires immediate attention._
+ * __Alert__ `(alert)`: _Immediate action is necessary to resolve a critical issue._
+ * __Critical__ `(crit)`: _Critical conditions in the program that demand intervention to prevent system failure._
+ * __Error__ `(error)`: _Error conditions that impair some operation but are less severe than critical situations._
+ * __Warning__ `(warn)`: _Potential issues that may lead to errors or unexpected behavior in the future if not addressed._
+ * __Notice__ `(notice)`: _Applies to normal but significant conditions that may require monitoring._
+ * __Informational__ `(info)`: _Includes messages that provide a record of the normal operation of the system._
+ * __Debug__ `(debug)`: _Logging detailed information about the system for debugging purposes._
 
 
 #### Credits
--- logger.go
+++ logger.go
@@ -22,28 +22,28 @@ import (
 	"time"
 )
 
-type LogLevel string
+type logLevel string
 
 const (
-	levelInfo      LogLevel = "info"
-	levelDebug     LogLevel = "debug"
-	levelNotice    LogLevel = "notice"
-	levelWarning   LogLevel = "warn"
-	levelAlert     LogLevel = "alert"
-	levelError     LogLevel = "error"
-	levelEmergency LogLevel = "emergency"
-	levelCritical  LogLevel = "crit"
-	levelFatal     LogLevel = "fatal"
+	levelInfo      logLevel = "info"
+	levelDebug     logLevel = "debug"
+	levelNotice    logLevel = "notice"
+	levelWarning   logLevel = "warn"
+	levelAlert     logLevel = "alert"
+	levelError     logLevel = "error"
+	levelEmergency logLevel = "emergency"
+	levelCritical  logLevel = "crit"
+	levelFatal     logLevel = "fatal"
 )
 
 type logger struct {
-	Level     LogLevel      `json:"level"`
+	Level     logLevel      `json:"level"`
 	Message   string        `json:"message"`
 	Trace     []interface{} `json:"trace"`
 	Timestamp string        `json:"timestamp"`
 }
 
-func log(level LogLevel, args []interface{}) *logger {
+func log(level logLevel, args []interface{}) *logger {
 	if len(args) == 0 {
 		return &logger{
 			Level:     level,
@@ -74,44 +74,53 @@ func log(level LogLevel, args []interface{}) *logger {
 	}
 }
 
-func write(level LogLevel, args []interface{}) {
+func write(level logLevel, args []interface{}) {
 	logContents, _ := json.Marshal(log(level, args))
 
 	println(string(logContents))
 }
 
+// LogInfo logs messages where in output JSON key "level" is "info"
 func LogInfo(args ...interface{}) {
 	write(levelInfo, args)
 }
 
+// LogDebug logs messages where in output JSON key "level" is "debug"
 func LogDebug(args ...interface{}) {
 	write(levelDebug, args)
 }
 
+// LogNotice logs messages where in output JSON key "level" is "notice"
 func LogNotice(args ...interface{}) {
 	write(levelNotice, args)
 }
 
+// LogWarning logs messages where in output JSON key "level" is "warn"
 func LogWarning(args ...interface{}) {
 	write(levelWarning, args)
 }
 
+// LogAlert logs messages where in output JSON key "level" is "alert"
 func LogAlert(args ...interface{}) {
 	write(levelAlert, args)
 }
 
+// LogEmergency logs messages where in output JSON key "level" is "emergency"
 func LogEmergency(args ...interface{}) {
 	write(levelEmergency, args)
 }
 
+// LogError logs messages where in output JSON key "level" is "error"
 func LogError(args ...interface{}) {
 	write(levelError, args)
 }
 
+// LogCritical logs messages where in output JSON key "level" is "crit"
 func LogCritical(args ...interface{}) {
 	write(levelCritical, args)
 }
 
+// LogFatal logs messages where in output JSON key "level" is "fatal"
 func LogFatal(args ...interface{}) {
 	write(levelFatal, args)
 }
